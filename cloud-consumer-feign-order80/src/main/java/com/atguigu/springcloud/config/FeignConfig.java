07058abb8a8cae1672408c274f19af31952c0f3d
王行行 <wanghanghang@huidaxuan.com>
2021-03-09 16:13:31 +0800

    openfeign日志增强

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.config;
 
 import feign.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfig {
 
+    @Bean
     public Logger.Level feignLoggerLevel(){
         return Logger.Level.FULL;
     }
