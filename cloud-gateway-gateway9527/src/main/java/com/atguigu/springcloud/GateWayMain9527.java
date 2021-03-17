23de243964372374e333e3352972c0954a064a75
wanghh <h510695134@163.com>
2021-03-17 21:47:45 +0800

    gateway工程搭建

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -2,18 +2,18 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: cloud2021
  * @description
  * @author: wanghh
- * @create: 2021-03-16 20:16
+ * @create: 2021-03-17 20:47
  **/
 @SpringBootApplication
-@EnableHystrixDashboard
-public class HystrixDashboardMain9001 {
+@EnableEurekaClient
+public class GateWayMain9527 {
     public static void main(String[] args) {
-        SpringApplication.run(HystrixDashboardMain9001.class,args);
+        SpringApplication.run(GateWayMain9527.class,args);
     }
 }
