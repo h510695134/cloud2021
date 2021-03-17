c7421c3f81ddf9e9183ea2fd1ffc856f01d8a7b5
wanghh <h510695134@163.com>
2021-03-17 20:13:03 +0800

    hystrix dashboard图形化界面

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud2021
+ * @description
+ * @author: wanghh
+ * @create: 2021-03-16 20:16
+ **/
+@SpringBootApplication
+
+public class HystrixDashboardMain9001 {
+}
