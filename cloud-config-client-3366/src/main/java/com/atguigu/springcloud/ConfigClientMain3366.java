6276c0848783248279eacdb6893af21736b13a06
wanghh <h510695134@163.com>
2021-03-25 20:55:33 +0800

    spring cloud config bus消息总线自动刷新

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2021
+ * @description
+ * @author: wanghh
+ * @create: 2021-03-24 20:50
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
