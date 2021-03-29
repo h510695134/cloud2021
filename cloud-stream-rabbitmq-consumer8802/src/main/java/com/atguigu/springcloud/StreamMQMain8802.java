34cd4d54671d609d8902e1114ddf2ed4ca031ec9
王行行 <wanghanghang@huidaxuan.com>
2021-03-29 15:14:56 +0800

    spring cloud stream rabbitmq consumer

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanghh
+ * @date 2021-03-29
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
