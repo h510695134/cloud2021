5955a8b9e74a23853842be3fc01ab33bffebb011
王行行 <wanghanghang@huidaxuan.com>
2021-03-29 18:01:09 +0800

    spring cloud alibaba nacos

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/OrderNacosMain83.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wanghh
+ * @date 2021-03-29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wanghh
+ * @date 2021-03-29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
