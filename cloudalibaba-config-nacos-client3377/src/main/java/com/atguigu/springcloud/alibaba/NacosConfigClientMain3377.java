92b594bf12ff5c532ae50955c1549c63c95279e9
王行行 <wanghanghang@huidaxuan.com>
2021-03-31 17:48:48 +0800

    spring cloud alibaba nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wanghh
+ * @date 2021-03-30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
