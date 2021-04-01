6ee0bf2d3bef1445664f54d2d0df3ef3b6e54982
王行行 <wanghanghang@huidaxuan.com>
2021-04-01 09:55:58 +0800

    测试合并代码

--- cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/controller/ConfigClientController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope // 支持nacos的动态刷新功能
 public class ConfigClientController {
 
-    @Value("${config.info}")
+    /*@Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/config/info")
     public String getConfigInfo(){
         return configInfo;
-    }
+    }*/
 }
