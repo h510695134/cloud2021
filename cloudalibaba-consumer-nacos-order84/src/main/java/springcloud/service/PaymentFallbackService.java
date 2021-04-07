bfab218765608947522b7ef81f4c543d86738079
王行行 <wanghanghang@huidaxuan.com>
2021-04-07 10:41:22 +0800

    sentinel熔断降级openfeign

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,17 @@
+package springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wanghh
+ * @date 2021-04-7
+ */
+@Component
+public class PaymentFallbackService implements PaymentService{
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id) {
+        return new CommonResult<>(444,"服务降级返回，-----------PaymentFallbackService",new Payment(id,"errorService"));
+    }
+}
---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/springcloud/service/PaymentService.java
@@ -0,0 +1,18 @@
+package springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author wanghh
+ * @date 2021-04-7
+ */
+@FeignClient(value = "nacos-payment-provider",fallback = PaymentFallbackService.class)
+public interface PaymentService {
+
+    @GetMapping("/paymentSQL/{id}")
+    CommonResult<Payment> paymentSQL(@PathVariable("id") Long id);
+}
